5bcb69e5e89014680915a660707cd5e95a8725eb
Harsh Rastogi <rastogiharsh04@gmail.com>
2022-12-02 20:05:49 +0530

    fix: insert router module

--- src/app/common/components/components.module.ts
+++ src/app/common/components/components.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { SharedModule } from './shared/shared.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-	declarations: [],
-	imports: [CommonModule, SharedModule],
-	exports: [SharedModule],
+	declarations: [SidebarComponent],
+	imports: [CommonModule, SharedModule, RouterModule],
+	exports: [SharedModule, SidebarComponent],
 })
 export class ComponentsModule {}
