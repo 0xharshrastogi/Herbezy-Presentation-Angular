a131e8337bb922a3ecc5779240dd9d0fea5b4377
Harsh Rastogi <rastogiharsh04@gmail.com>
2022-12-04 13:49:49 +0530

    create dashboard home component

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ComponentsModule } from '@common/components/components.module';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { RootComponent } from './root/root.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-	declarations: [RootComponent],
-	imports: [CommonModule, DashboardRoutingModule],
+	declarations: [RootComponent, HomeComponent],
+	imports: [CommonModule, DashboardRoutingModule, ComponentsModule],
 })
 export class DashboardModule {}
